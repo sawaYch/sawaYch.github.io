2bc7577963a127732e1e5fbe90550e82e267c43c
sawaYch <chyeungam@connect.ust.hk>
2018-12-06 06:27:02 +0800

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import '../components/layout_404.css'
+import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <>
-    <div class="q">
-      <div class="c">
-        <div class="_404">404</div>
-        <div class="_1">THE PAGE</div>
-        <div class="_2">WAS NOT FOUND</div>
-        <a class="btn" href="/">
+  <Layout>
+    <div className='q'>
+      <div className='c'>
+        <div className='_404'>404</div>
+        <div className='_1'>THE PAGE</div>
+        <div className='_2'>WAS NOT FOUND</div>
+        <a className='btn' href='/'>
           BACK TO THE EARTH
         </a>
       </div>
-      </div>
-  </>
+    </div>
+  </Layout>
 )
 
 export default NotFoundPage
