49b74f58b9edc1ead284639c965e7a6fcbea30f7
sawaYch <chyeungam@connect.ust.hk>
2018-12-28 03:54:14 +0800

    fix
    fuck i know the problem
    page must have layout component lol

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import PageTransition from 'gatsby-plugin-page-transitions';
 import { IoIosArrowDropleftCircle } from "react-icons/io";
 import {Radar} from 'react-chartjs-2';
 import LazyLoad from 'react-lazyload';
+import Layout from '../components/layout'
 
 const data = {
   labels: ['Embedded System', 'FrontEnd', 'BackEnd', 'Language', 'Algorithm'],
@@ -41,30 +42,31 @@ const AboutPage = () => (
       }}
       transitionTime={500}
       >
-      <div className='container'>
-        <div className='aboutTopBanner' />
-        <h2><center>Something About Me</center></h2>
-        <div className='doc'>
-          <p className='part1'> 
+      <Layout>
+        <div className='container'>
+          <div className='aboutTopBanner' />
+          <h2 className='abttopic'><center>Something About Me</center></h2>
+          <div className='doc'>
+            <p className='part1'> 
             Hi ! I'm Sawa, a Computer Science Engineering Student from Hong Kong. 
             Love to combine technology with creative ideas. 
-          </p>
-          <p className='part2'>
+            </p>
+            <p className='part2'>
             I'm currently doing my bachelor degree in The Hong Kong University of Science and 
             Technology. I am slightly obsessed with algorithm & CTF. Also with background in
             Network and System Administration (and I love Linux 😎) 
-          </p>
-          <p className='part2'>
+            </p>
+            <p className='part2'>
             Furthermore, I know a little bit about Robotics and Embedded System.
             When I’m not coding, I enjoy reading manga, watching anime and playing console games 😀
             Sometimes I go cycling and badminton with my buddies.
-          </p>
-        </div>
-        <h2><center>Skills</center></h2>
-        <LazyLoad height={'50vh'}>
-          <div className='chartBox'>
-            <Radar data={data}
-              options={{
+            </p>
+          </div>
+          <h2 className='abttopic'><center>Skills</center></h2>
+          <LazyLoad height={'50vh'}>
+            <div className='chartBox'>
+              <Radar data={data}
+                options={{
                   maintainAspectRatio: false,
                   legend: {display: false},
                   scale: {
@@ -77,31 +79,32 @@ const AboutPage = () => (
                   }
                 }}
                 />
-          </div>
-        </LazyLoad>
-        <ul>
-          <li>Language: HTML5, CSS3, Javascript, ES5, PHP, SQL, LaTex, Lua, C, C++, C#, Java, Python, Visual Basic, Delphi(Pascal)</li>
-          <li>Framework: GatsbyJs, Jekyll, ReactJs, Flask, CodeIgniter</li>
-          <li>Others: Debian & Arch-branch Linux, Git, Bash, Docker, STM32 Standard Peripheral Library, Qt, Gtk, WPF, WordPress CMS</li>
-        </ul>
-        <h2><center>Experiences</center></h2>
-        <div className='listtype2'>
+            </div>
+          </LazyLoad>
           <ul>
-            <li><a>2017-2018 HKUST Robotics Team Member - Robocon (War Dragon)</a></li>
-            <li><a>2017 Inter-Tertiary-Institute Capture the Flag (CTF) contest ASTRI x iChunQiu</a></li>
-            <li><a>2016 Microsoft Azure Training Program - Cloud Admin & Apps on Cloud Platform</a></li>
+            <li>Language: HTML5, CSS3, Javascript, ES5, PHP, SQL, LaTex, Lua, C, C++, C#, Java, Python, Visual Basic, Delphi(Pascal)</li>
+            <li>Framework: GatsbyJs, Jekyll, ReactJs, Flask, CodeIgniter</li>
+            <li>Others: Debian & Arch-branch Linux, Git, Bash, Docker, STM32 Standard Peripheral Library, Qt, Gtk, WPF, WordPress CMS</li>
           </ul>
-        </div>
+          <h2 className='abttopic'><center>Experiences</center></h2>
+          <div className='listtype2'>
+            <ul>
+              <li><a>2017-2018 HKUST Robotics Team Member - Robocon (War Dragon)</a></li>
+              <li><a>2017 Inter-Tertiary-Institute Capture the Flag (CTF) contest ASTRI x iChunQiu</a></li>
+              <li><a>2016 Microsoft Azure Training Program - Cloud Admin & Apps on Cloud Platform</a></li>
+            </ul>
+          </div>
 
-        <div className='aboutback'>
-          <Link to='/'>
-            <IoIosArrowDropleftCircle size={'7vh'} />
-          </Link>
-        </div>        
-      </div>
-      <div className='footer'>
+          <div className='aboutback'>
+            <Link to='/'>
+              <IoIosArrowDropleftCircle size={'7vh'} />
+            </Link>
+          </div>        
+        </div>
+        <div className='footer'>
           sawaYch@2018 v1.0.3
-      </div>
+        </div>
+      </Layout>
     </PageTransition>
     <SideBar />    
   </div>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,15 +2,14 @@ import React from 'react'
 import SideBar from '../components/sidebar'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
 
 const Blog = () => (
-  <Layout>
+  <div>
     {/* <h1>Here is the blog page</h1>
     <p>Welcome to my blog</p>
     <Link to='/'>Go back to the homepage</Link> */}
     <SideBar />
-  </Layout>
+  </div>
 )
 
 export default Blog
